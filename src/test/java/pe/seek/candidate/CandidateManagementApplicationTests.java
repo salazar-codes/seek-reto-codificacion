8778d2f5beaeec2bc7be4ce66b82aafc3e7e0035
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-11-28 01:59:46 -0500

    first commit

---
+++ src/test/java/pe/seek/candidate/CandidateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.seek.candidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
